6fe06edcf6aedf681d84279ee73d16800a766b25
Scott Bowler <scott.bowler@learningfuze.com>
2022-02-17 20:07:09 +0000

    Removed console log

--- client/pages/not-found.jsx
+++ client/pages/not-found.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 export default function NotFound() {
   return (
     <div className="d-flex" style={{ height: '100vh' }}>
-      <h1 className="m-auto">
-        <span className="text-danger">404</span> - Page Not Found
-      </h1>
+      <div className="m-auto text-center">
+        <h1>
+          <span className="text-danger">404</span> - Page Not Found
+        </h1>
+        <a href="#">Return Home</a>
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 require('dotenv/config');
+const db = require('./db');
 const express = require('express');
 const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
@@ -6,6 +7,26 @@ const staticMiddleware = require('./static-middleware');
 const app = express();
 
 app.use(staticMiddleware);
+
+app.get('/api/entries', (req, res, next) => {
+  const userId = 1;
+
+  const sql = `
+    select
+      "entryId", title, "photoUrl", notes
+    from 
+      entries
+    where
+      "userId" = $1;
+  `;
+  const params = [userId];
+
+  db.query(sql, params)
+    .then(resp => {
+      res.json(resp.rows);
+    })
+    .catch(err => next(err));
+});
 
 app.use(errorMiddleware);
 
