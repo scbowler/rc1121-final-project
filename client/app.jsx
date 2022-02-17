998e53aecebfdb4e031f81f77e1c6fba6f53a85f
Scott Bowler <scott.bowler@learningfuze.com>
2022-02-17 19:19:04 +0000

    Initial setup complete

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,42 @@
 import React from 'react';
 import Home from './pages/home';
+import NotFound from './pages/not-found';
+import { parseRoute } from './lib';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      route: parseRoute(location.hash)
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      this.setState({
+        route: parseRoute(location.hash)
+      });
+    });
+  }
+
+  renderPage() {
+    const { route } = this.state;
+
+    if (route.path === '') {
+      return <Home />;
+    }
+
+    return <NotFound />;
+  }
+
   render() {
-    return <Home />;
+    return (
+      <>
+        <div className="container">
+          {this.renderPage()}
+        </div>
+      </>
+    );
   }
 }
---
+++ client/pages/not-found.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <div className="d-flex" style={{ height: '100vh' }}>
+      <h1 className="m-auto">
+        <span className="text-danger">404</span> - Page Not Found
+      </h1>
+    </div>
+  );
+}
